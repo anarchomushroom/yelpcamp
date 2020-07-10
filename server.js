62a91665387f4136c04b484299d56bd8795feb0d
anarchomushroom <prushguitar@gmail.com>
2020-07-10 18:26:24 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// ===================================================================
+// ROUTES
+// ===================================================================
+
+// Root
+app.get('/', function(req, res) {
+	res.render('landing');
+});
+
+// Campgrounds
+app.get('/campgrounds', function(req, res) {
+	let campgrounds = [
+		{
+			name: 'Salmon Creek',
+			image: 'https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&h=350'
+		},
+		{
+			name: 'Granite Hill',
+			image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'
+		},
+		{
+			name: "Mountain Goat's Rest",
+			image: 'https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712c7fd29f4dc1_340.jpg'
+		}
+	];
+
+	res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+// Server Start
+app.listen(5000, function() {
+	console.log('Server has started at localhost:5000');
+});
