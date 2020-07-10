70e4ce0ac2a6690f9e73be1495e6cfef62ab2eaa
anarchomushroom <prushguitar@gmail.com>
2020-07-10 19:15:33 +0100

    Add logic to make new campground

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+let campgrounds = [
+	{
+		name: 'Salmon Creek',
+		image: 'https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&h=350'
+	},
+	{
+		name: 'Granite Hill',
+		image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'
+	},
+	{
+		name: "Mountain Goat's Rest",
+		image: 'https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712c7fd29f4dc1_340.jpg'
+	}
+];
+
 // ===================================================================
 // ROUTES
 // ===================================================================
@@ -16,29 +31,18 @@ app.get('/', function(req, res) {
 
 // Campgrounds/GET
 app.get('/campgrounds', function(req, res) {
-	let campgrounds = [
-		{
-			name: 'Salmon Creek',
-			image: 'https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&h=350'
-		},
-		{
-			name: 'Granite Hill',
-			image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'
-		},
-		{
-			name: "Mountain Goat's Rest",
-			image: 'https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712c7fd29f4dc1_340.jpg'
-		}
-	];
-
 	res.render('campgrounds', { campgrounds: campgrounds });
 });
 
 // Campgrounds/POST
 app.post('/campgrounds', function(req, res) {
-	res.send('This is the post route');
 	// get data from form and add to campgrounds array
+	let name = req.body.name;
+	let image = req.body.image;
+	let newCampground = { name: name, image: image };
+	campgrounds.push(newCampground);
 	//redirect back to campgrounds page
+	res.redirect('/campgrounds');
 });
 
 // New Campground/GET
