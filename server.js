a1ac307da5b5be9edb1d2c759105f6029f3f3656
anarchomushroom <prushguitar@gmail.com>
2020-07-11 02:39:11 +0100

    Render campgrounds from DB

--- server.js
+++ server.js
@@ -37,21 +37,6 @@ Campground.create(
 	}
 );
 
-let campgrounds = [
-	{
-		name: 'Salmon Creek',
-		image: 'https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&h=350'
-	},
-	{
-		name: 'Granite Hill',
-		image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350'
-	},
-	{
-		name: "Mountain Goat's Rest",
-		image: 'https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440712c7fd29f4dc1_340.jpg'
-	}
-];
-
 // ===================================================================
 // ROUTES
 // ===================================================================
@@ -63,7 +48,14 @@ app.get('/', function(req, res) {
 
 // Campgrounds/GET
 app.get('/campgrounds', function(req, res) {
-	res.render('campgrounds', { campgrounds: campgrounds });
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, campgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds: campgrounds });
+		}
+	});
 });
 
 // Campgrounds/POST
