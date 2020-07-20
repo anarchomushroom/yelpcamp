98de11e17cf173a042d1f5b398461db024475811
anarchomushroom <prushguitar@gmail.com>
2020-07-20 17:39:03 +0100

    Add registration functionality

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const Comment = require('./models/comment');
 const User = require('./models/user');
 const seedDB = require('./seeds');
 
-seedDB();
+// Commented out to prevent seeding on DB load
+// seedDB();
 
 mongoose
 	.connect('mongodb://localhost/yelp_camp', {
@@ -22,6 +23,22 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
+
+// ===================================================================
+// PASSPORT CONFIG
+// ===================================================================
+app.use(
+	require('express-session')({
+		secret: 'Always Be Running',
+		resave: false,
+		saveUninitialized: false
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // ===================================================================
 // ROUTES
@@ -115,6 +132,26 @@ app.post('/campgrounds/:id/comments', function(req, res) {
 	// create new comment
 	// connect new comment to campground
 	// redirect campground show page
+});
+
+// ====================================
+// AUTH ROUTES
+// ====================================
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+app.post('/register', function(req, res) {
+	let newUser = new User({ username: req.body.username });
+	User.register(newUser, req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+			return res.render('register');
+		}
+		passport.authenticate('local')(req, res, function() {
+			res.redirect('/campgrounds');
+		});
+	});
 });
 
 // Server Start
