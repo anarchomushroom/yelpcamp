f34c0945f6ea47de0c0f40b3c0b0e63538717176
anarchomushroom <prushguitar@gmail.com>
2020-07-27 15:22:50 +0100

    Add flash message to header partial

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post('/register', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
-	res.render('login', { message: req.flash('error') });
+	res.render('login');
 });
 
 router.post(
